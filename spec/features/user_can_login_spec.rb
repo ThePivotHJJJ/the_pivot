26415bfb6a76f4b619d5cfcea10fb6ce95678c5d
Julian Feliciano <julsfelic@gmail.com>
2016-03-22 16:52:27 -0600

    Add sad path test for user login
    Added a test for a user trying to sign in with invalid information.

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -18,4 +18,25 @@ feature "User can login" do
     expect(page).to_not have_content("login")
     expect(page).to have_content("logout")
   end
+
+  context "with invalid information" do
+    scenario "they see an error message" do
+      FactoryGirl.create(:user)
+      user = User.first
+
+      visit root_path
+      click_on "login"
+      fill_in "Username", with: user.username
+      fill_in "Password", with: ""
+      within ".login" do
+        click_on "Login"
+      end
+
+      expect(current_path).to_not eq("/dashboard")
+      expect(page).to_not have_content("logged_in_as_#{user.username}")
+      expect(page).to have_content("login")
+      expect(page).to_not have_content("logout")
+      expect(page).to have_content("Invalid Login")
+    end
+  end
 end
