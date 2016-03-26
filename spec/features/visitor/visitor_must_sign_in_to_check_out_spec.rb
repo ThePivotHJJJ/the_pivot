7ae534f3f4fc7f6aa9c1fcae8ab3bcc4bd95d508
July Tran <julyytran@gmail.com>
2016-03-25 19:43:08 -0600

    passing test for visitor must sign in to check out

---
+++ spec/features/visitor/visitor_must_sign_in_to_check_out_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+feature "visitor must sign in to check out" do
+  scenario "they are directed to login page when trying to check out" do
+    shop1 = Shop.create(name: "Gifs for Good")
+    item1 = create(:item)
+    shop1.items << item1
+
+    visit shops_path
+
+    click_on "Gifs for Good"
+    expect(current_path).to eq shop_path(shop: shop1.slug)
+
+    click_on item1.title
+    click_on "Add to cart"
+
+    visit cart_path
+
+    click_on "Checkout"
+
+    expect(current_path).to eq login_path
+    expect(page).to have_button "Login"
+    expect(page).to have_link "Haven't Registered? Sign up here!"
+  end
+end
