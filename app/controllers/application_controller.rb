7fd8f50b287cacc6f83a1fb2b63b8452ceeea343
July Tran <julyytran@gmail.com>
2016-03-27 17:48:20 -0600

    add permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_cart
+  before_action :authorize!
   helper_method :current_user, :format_price, :find_item
 
   def set_cart
@@ -12,6 +13,20 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_permission
+    @current_permission ||= Permission.new(current_user)
+  end
+
+  def authorize!
+    unless authorized?
+      redirect_to root_url, danger: "You are not allow to visit this page."
+    end
+  end
+
+  def authorized?
+    current_permission.allow?(params[:controller], params[:action])
+  end
+  
   def find_item
     Item.find(params[:id])
   end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,9 @@
+module ApplicationHelper
+  def platform_admin?
+    current_user && current_user.platform_admin?
+  end
+
+  def business_admin?
+    current_user && current_user.business_admin?
+  end
+end
