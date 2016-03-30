72f2d4bbda70d046e4f9a88fc2bf146eab246a1f
July Tran <julyytran@gmail.com>
2016-03-29 21:35:14 -0600

    add statuses for lost and open bids

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -3,8 +3,11 @@ module ItemsHelper
     if bid.item.retired?
       if bid.item.max_bid_user == current_user
         link_to "Won", cart_items_path(item_id: bid.item.id), method: :post
+      else
+        "Lost"
       end
     else
+      "Open"
     end
   end
 end
