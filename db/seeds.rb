c5caef68b85fa604e338ae755acaa975766bbfc6
Julian Feliciano <julsfelic@gmail.com>
2016-03-30 13:25:58 -0600

    Add role generation to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,70 +1,38 @@
-# class Seed
-#   def start
-#     create_tags
-#     create_items
-#     create_users
-#     create_orders
-#     create_order_items
-#   end
+class Seed
+  def self.start
+    seed = Seed.new
+    seed.generate_roles
+    # seed.generate_platform_admin
 
-#   def create_tags
-#     10.times { Tag.create!(name: Faker::Hipster.word)}
-#   end
-
-#   def create_items
-#     10.times do
-#       Item.create!(title: Faker::Lorem.sentence,
-#         description: Faker::Hipster.word,
-#         price: Random.rand(1..1000),
-#         image: Faker::Avatar.image
-#         tags: Tag.all.sample)
-#   end
-
-#   def create_users
-#     50.times do
-#       User.create!(username: Faker::Internet.user_name,
-#         password: Faker::Internet.password)
-#     end
-#   end
-
-#   def create_orders
-
-#   end
-
-#   def create_order_items
-
-#   end
-# end
-
-# Seed.new.start
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # 20 total businesses
+    # 10 tags
+    # 50 items per tags
+    # 100 registered customers, one with the following data:
+    #   10 orders per registered customer
+    #   1 business admins per business
+    #
+    #   Username: josh@turing.io
+    #   Password: password
+    #
+    #   1 business admin
+    #   Username: andrew@turing.io
+    #   Password: password
+    #
+    #   1 platform administrators
+    #   Username: jorge@turing.io
+    #   Password: password
 
 
-# 10.times do
-#   Tag.create(name: Faker::Hipster.word)
-# end
-# #
-# 71.times do
-#   item = Item.create(title: Faker::Lorem.sentence,
-#               description: " adfg",
-#               price: 100,
-#               image: "https://placeholdit.imgix.net/~text?txtsize=60&bg=000000&txt=640%C3%97480&w=640&h=480&fm=png"
-#               )
-#   item.tags << Tag.all.shuffle.first
-# end
-#
-#
-# 20.times do
-#   subtotal = Random.new.rand(1..10)
-#   user = User.create(username: "string", password: "password")
-#   item = Item.all.shuffle.first
-#   order = user.orders.create!(total_price: 3*subtotal)
-#   order.order_items.create!(
-#     item_id: item.id, quantity: 1, subtotal: subtotal
-#   )
-#   item = Item.all.shuffle.first
-#   order.order_items.create!(
-#     item_id: item.id, quantity: 2, subtotal: subtotal*2
-#   )
-# end
+  end
+
+  def generate_roles
+    Role.create(name: "registered_user")
+    Role.create(name: "business_admin")
+    Role.create(name: "platform_admin")
+  end
+
+  def generate_platform_admin
+  end
+end
+
+Seed.start
