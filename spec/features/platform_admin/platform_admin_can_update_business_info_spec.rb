c6c45f797b2756182d01b0bfbac7e18c8878071a
July Tran <julyytran@gmail.com>
2016-03-31 11:49:21 -0600

    sad path for platform shop update

--- spec/features/platform_admin/platform_admin_can_update_business_info_spec.rb
+++ spec/features/platform_admin/platform_admin_can_update_business_info_spec.rb
@@ -28,4 +28,28 @@ feature "platform admin can update a shop's name" do
       expect(page).not_to have_content("Double J's Yummy Snack Party")
     end
   end
+
+  scenario "invalid name input" do
+    create_roles
+    create_business_admin_and_shop
+    logout
+    create_platform_admin
+
+    expect(current_path).to eq platform_admin_dashboard_path
+
+    click_on "Manage Stores"
+
+    expect(current_path).to eq platform_admin_shops_path
+
+    within("#shop-#{Shop.all.last.id}") do
+      expect(page).to have_content("Double J's Yummy Snack Party")
+      click_on "Edit Shop"
+    end
+
+    fill_in "Name", with: ""
+    click_on "Save"
+
+    expect(current_path).to_not eq platform_admin_shops_path
+    expect(page).to have_content "Invalid input"
+  end
 end
