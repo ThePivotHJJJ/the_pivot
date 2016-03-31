4af15befdcc8acede952100657b41d8ad4d1688c
July Tran <julyytran@gmail.com>
2016-03-31 11:45:43 -0600

    sad path for item create and update

--- spec/features/business_admin/business_admin_can_update_item_spec.rb
+++ spec/features/business_admin/business_admin_can_update_item_spec.rb
@@ -16,4 +16,20 @@ feature "business admin can update shop item" do
     expect(page).to have_content "Bunnies"
     expect(page).to_not have_content "all of teh lulz"
   end
+
+  scenario "invalid item input" do
+    create_business_admin_and_shop
+    create_shop_item
+    item = Item.find_by(title: "all of teh lulz")
+
+    visit shop_item_path(shop: item.shop.slug, id: item.id)
+
+    click_on "Edit"
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: ""
+    click_on "Update item"
+
+    expect(page).to have_content "Invalid input"
+  end
 end
