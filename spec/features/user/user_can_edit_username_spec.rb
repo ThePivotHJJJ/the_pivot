648ae594d771402867d71fa91460be6109f071af
July Tran <julyytran@gmail.com>
2016-03-31 13:24:42 -0600

    sad path user update

--- spec/features/user/user_can_edit_username_spec.rb
+++ spec/features/user/user_can_edit_username_spec.rb
@@ -17,4 +17,21 @@ feature "user can edit their username" do
     expect(current_path).to eq dashboard_path
     expect(page).to have_content "Welcome, Ash"
   end
+
+  scenario "invalid input" do
+    create_roles
+    create_registered_user
+    user = User.find_by(username: "Misty")
+    login(user)
+
+    visit dashboard_path
+
+    click_on "Edit My Profile"
+
+    fill_in "Username", with: ""
+    click_on "Save"
+
+    expect(current_path).to_not eq dashboard_path
+    expect(page).to_not have_content "Welcome, Ash"
+  end
 end
