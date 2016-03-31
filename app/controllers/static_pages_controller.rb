5933e2204946c8203f10b1a738cbe58e65890448
Julian Feliciano <julsfelic@gmail.com>
2016-03-30 19:26:02 -0600

    Add dynamic discover links in footer
    Added functionality in the footer that dynamically adds tags in the
    footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def landing
     @featured_items = Item.limit(4).order("RANDOM()")
+    @featured_tags = Tag.limit(4).order("RANDOM()")
   end
 end
