3cd9ab39d6c89ab86e8f77001232d6f54e9754bd
Julian Feliciano <julsfelic@gmail.com>
2016-03-30 21:25:51 -0600

    Finish styling and refactoring landing page
    Finalize the styling and refactor the view for tomorrows check in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def landing
     @featured_items = Item.limit(4).order("RANDOM()")
-    @featured_tags = Tag.limit(4).order("RANDOM()")
   end
 end
